f97718e3c81ff738d8301108a93d855027b81b24
Aster Bandis <68644945+bandisast@users.noreply.github.com>
2023-11-06 22:11:56 +0200

    Add IEM secretary data

--- src/assets/data/secretaries.js
+++ src/assets/data/secretaries.js
@@ -39,53 +39,11 @@ import i18n from "../../i18n";
 
 const secrData = [
   {
-    name: i18n.t("eco_department"),
-    email: "ecosecr@uom.edu.gr",
-    tel: "2310 891.119, 149, 211, 265, 656",
-    link: "https://www.uom.gr/eco/grammateia",
-  },
-  {
-    name: i18n.t("bso_department"),
-    email: "bsosecr@uom.gr",
-    tel: "2310891375, 2310891645, 2310891433",
-    link: "https://www.uom.gr/bso/grammateia",
-  },
-  {
-    name: i18n.t("dai_department"),
-    email: "daisecr@uom.edu.gr",
-    tel: "2310.891 158",
-    link: "https://www.uom.gr/dai/grammateia",
-  },
-  {
-    name: i18n.t("ba_department"),
-    email: "basecr@uom.edu.gr",
-    tel: "2310 891.283, 2310 891.213, 2310 891.646, 2310 891.214",
-    link: "https://www.uom.gr/ba/grammateia",
-  },
-  {
-    name: i18n.t("fin_department"),
-    email: "finsecr@uom.edu.gr",
-    tel: "2310891607, 2310891441",
-    link: "https://www.uom.gr/fin/grammateia",
-  },
-  {
-    name: i18n.t("ies_department"),
-    email: "essecr@uom.edu.gr",
-    tel: "2310 891.285",
-    link: "https://www.uom.gr/ies/grammateia",
-  },
-  {
-    name: i18n.t("esp_department"),
-    email: "espsecr@uom.edu.gr",
-    tel: "2310.891258, 2310891339, 2310 891339, 2310 891253",
-    link: "https://www.uom.gr/esp/epikoinonia",
-  },
-  {
-    name: i18n.t("msa_department"),
-    email: "msasecr@uom.edu.gr",
-    tel: "2310 891.281, 2310 891.902",
-    link: " https://www.uom.gr/msa/grammateia-tmhmatos",
-  },
+    name: i18n.t("iem_department"),
+    email: "info@iem.ihu.gr",
+    tel: "2310-013.939, 2310-013.940",
+    link: "https://iem.ihu.gr/grammateia.php",
+  }
 ];
 
 export default secrData;
